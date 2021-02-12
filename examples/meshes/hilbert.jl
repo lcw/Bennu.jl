21787f92ac044548dce341abca5344fd88455761
Lucas C Wilcox <lucas@swirlee.com>
2021-02-11 19:59:24 -0800

    Add a mesh partitioning example
    This example uses a Hilbert curve through the centroids of the elements
    of a Cartesian mesh to partition it.

---
+++ examples/meshes/hilbert.jl
@@ -0,0 +1,20 @@
+using Bennu
+using Meshes
+using Plots
+unicodeplots()
+
+mesh = CartesianGrid((4, 4), (10., 10.), (0.1, 0.1))
+
+u = coordinates(minimum(mesh))
+v = coordinates(maximum(mesh))
+integercoordinates(c) = quantize.((c .- u) ./ (v .- u))
+
+centroids = coordinates.(Meshes.center.(elements(mesh)))
+centroids = centroids[sortperm(hilbertcode.(integercoordinates.(centroids)))]
+
+p = plot(mesh)
+for r in partition(1:length(centroids), 5)
+  plot!(p, Tuple.(centroids[r]))
+end
+display(p)
+println()
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,30 @@
 using Bennu
 using Test
+using Pkg: Pkg
 
 using FastGaussQuadrature: FastGaussQuadrature
 
 include("operators.jl")
 include("partitions.jl")
 include("quadratures.jl")
+
+@testset "examples" begin
+    julia = Base.julia_cmd()
+    base_dir = joinpath(@__DIR__, "..")
+
+    for example_dir in readdir(joinpath(base_dir, "examples"), join=true)
+        @testset "$example_dir" begin
+            mktempdir() do tmp_dir
+                example_project = Pkg.Types.projectfile_path(example_dir)
+                tmp_project = Pkg.Types.projectfile_path(tmp_dir)
+                cp(example_project, tmp_project)
+
+                for script in filter!(s->endswith(s, ".jl"),
+                                      readdir(example_dir, join=true))
+                    cmd = `$julia --project=$tmp_project -e "import Pkg; Pkg.develop(path=raw\"$base_dir\"); Pkg.resolve(); Pkg.instantiate(); include(raw\"$script\")"`
+                    @test success(pipeline(cmd, stderr=stderr, stdout=stdout))
+                end
+            end
+        end
+    end
+end
