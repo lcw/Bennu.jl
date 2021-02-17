8a44e08c2a5eec7e91dd44accfddcffb52579ea5
Lucas C Wilcox <lucas@swirlee.com>
2021-02-17 11:48:09 -0800

    Teach three term similar to use array types
    This allows `similar` to use array types (not just arrays) as the first
    argument.

--- src/arrays.jl
+++ src/arrays.jl
@@ -1,2 +1,5 @@
+Base.similar(::Type{A}, ::Type{T}, dims...) where {A <: AbstractArray, T} =
+    similar(similar(A, 0), T, dims...)
+
 Tullio.storage_type(s::StructArray) =
     promote_type(map(Tullio.storage_type, StructArrays.components(s))...)
--- test/arrays.jl
+++ test/arrays.jl
@@ -1,4 +1,8 @@
 @testset "arrays" begin
+    @test eltype(similar(Array{Bool}, Int32, (3,2))) == Int32
+    @test similar(Array{Float64}, Float32, (1,2)) isa Array
+    @test size(similar(Array{Int}, Int8, (4,2))) == (4,2)
+
     tup = ntuple(i->SizedArray{Tuple{3,1,2}}(rand(3,1,2)), 4)
     A = StructArray{SVector{4,Float64}}(data = StructArray(tup))
     @test Tullio.storage_type(A) <: Array
