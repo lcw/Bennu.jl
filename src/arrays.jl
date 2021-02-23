06bdb6424b0eed5edc5015fc255f97f0b49e28ca
Lucas C Wilcox <lucas@swirlee.com>
2021-02-23 14:11:28 -0800

    Add StructArray{StaticArray} constructors
    This adds constructors for `StructArray`s with element type
    `StaticArray` and overloads `Base.similar` for these arrays.

--- src/arrays.jl
+++ src/arrays.jl
@@ -11,3 +11,24 @@ function components(s::StructArray{S,
                                    I}) where {S <: StaticArray, T, N, C, I}
     return StructArrays.components(StructArrays.components(s).data)
 end
+
+function Base.similar(s::StructArray{S, N,
+                                     NamedTuple{(:data,),
+                                                Tuple{StructArray{T, N, C, I}}},
+                                     I},
+                      element_type::Type{E}=S,
+                      dims::Tuple{Vararg{Int64, M}}=size(s)) where
+                          {S<:StaticArray, T, N, C, I, E, M}
+    cs = ntuple(i->similar(first(components(s)), dims), length(element_type))
+    return structarray(element_type, cs)
+end
+
+function structarray(components::Tuple)
+    T = promote_type(eltype.(components)...)
+    N = length(components)
+    return structarray(SVector{N,T}, components)
+end
+
+function structarray(::Type{T}, components::Tuple) where {T}
+    return StructArray{T}(data = StructArray(components))
+end
--- test/arrays.jl
+++ test/arrays.jl
@@ -8,4 +8,38 @@
     @test Tullio.storage_type(A) <: Array
     @test Tullio.storage_type(SizedArray{Tuple{3,1,2}}(A)) <: Array
     @test components(A) == tup
+
+    data = ntuple(i->rand(3,4), 4)
+
+    A = Bennu.structarray(data)
+    @test all(components(A) .=== data)
+    @test A isa StructArray
+    @test eltype(A) == SVector{4,Float64}
+    @test size(A) == (3,4)
+
+    A = Bennu.structarray(SMatrix{2,2,Float64}, data)
+    @test all(components(A) .=== data)
+    @test A isa StructArray
+    @test eltype(A) == SMatrix{2,2,Float64}
+    @test size(A) == (3,4)
+
+    B = similar(A)
+    @test B isa StructArray
+    @test eltype(B) == SMatrix{2,2,Float64}
+    @test size(B) == (3,4)
+
+    B = similar(A, (1,1,3))
+    @test B isa StructArray
+    @test eltype(B) == SMatrix{2,2,Float64}
+    @test size(B) == (1,1,3)
+
+    B = similar(A, SVector{3,Float32})
+    @test B isa StructArray
+    @test eltype(B) == SVector{3,Float32}
+    @test size(B) == (3,4)
+
+    B = similar(A, SVector{3,Float32}, (2,3,1))
+    @test B isa StructArray
+    @test eltype(B) == SVector{3,Float32}
+    @test size(B) == (2,3,1)
 end
