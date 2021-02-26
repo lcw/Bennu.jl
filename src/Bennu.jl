be72ae88866bb04251a8d7a4bb4c72ee5704b514
Lucas C Wilcox <lucas@swirlee.com>
2021-02-25 19:18:24 -0800

    Add various dependencies
    These are the packages that will be used to build and output a basic
    spectral element grid.

--- src/Bennu.jl
+++ src/Bennu.jl
@@ -1,7 +1,17 @@
 module Bennu
 
+using Adapt
+using CUDA
+using FillArrays
+using KernelAbstractions
+using LazyArrays
+using LinearAlgebra
+using LoopVectorization
+using StaticArrays
+using StaticArrays: tuple_prod, tuple_length, size_to_tuple
 using StructArrays
 using Tullio
+using WriteVTK
 
 export spectralderivative, spectralinterpolation, legendregauss,
        legendregausslobatto, partition, hilbertcode, quantize
