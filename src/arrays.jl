b8fe6a0317568cb3512f38a2391fdf3b7b953a5c
Lucas C Wilcox <lucas@swirlee.com>
2021-03-22 14:33:26 -0700

    Add function to contiguously number array entries

--- src/arrays.jl
+++ src/arrays.jl
@@ -2,3 +2,15 @@ arraytype(A) = Tullio.storage_type(A) <: CuArray ? CuArray : Array
 arraytype(::Type{T}) where {T} = Array
 arraytype(::Type{<:CuArray}) = CuArray
 arraytype(::Type{<:CUDA.Adaptor}) = CuArray
+
+function numbercontiguous(A; by=identity)
+    p = sortperm(A; by=by)
+    notequalprevious = fill!(similar(p, Bool), false)
+    @tullio notequalprevious[i] =
+        @inbounds(begin by(A[p[i]]) != by(A[p[i-1]]) end) (i in 2:length(p))
+
+    B = similar(p)
+    B[p] .= cumsum(notequalprevious) .+ 1
+
+    return B
+end
--- test/arrays.jl
+++ test/arrays.jl
@@ -3,4 +3,6 @@
     if CUDA.has_cuda_gpu()
         @test arraytype(CuArray(ones(3, 5))) === CuArray
     end
+
+    @test [1, 3, 1, 2, 1, 2] == Bennu.numbercontiguous([10, 29, 10, 23, 10, 23])
 end
